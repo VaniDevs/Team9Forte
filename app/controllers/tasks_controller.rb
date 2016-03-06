5616bfaef1da4bd88bd9cf88b427d85b01ead215
Stephanie Beaton <stefanybeaton@gmail.com>
2016-03-05 19:41:39 -0800

    localhost:3000/tasks/new  displays form and save button saved to database and displays task

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -3,9 +3,44 @@ class TasksController < ApplicationController
 		@tasks = Task.all
 	end
 
+
   def new
+
+    @task = Task.new
+
   end
 
   def create
+
+    debugger
+
+    #  redirect to tasks_path if current_user is not an employer
+    employer = Employer.find(current_user.id)
+
+    if !employer
+      redirect_to tasks_path
+    end
+
+    # find user,  assuming they logged on as employer
+    employer = current_user
+
+    @task = Task.new(task_params)
+
+    employer.tasks << @task
+
+    if employer.save
+      redirect_to tasks_path
+    else
+      render :new
+    end
   end
+
+  protected
+
+  # security issue, Rails has adopted a standard called "strong parameters" for mass assignment.
+  def task_params
+    params.require(:task).permit(:title, :description, :address, :start_date, :end_date, :duration)
+
+  end
+
 end
