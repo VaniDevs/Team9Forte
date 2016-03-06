b0e9952049c33842b0c0c5f362a814f2a6d3db48
Nicholas Pan <nwpan@sfu.ca>
2016-03-06 10:54:12 -0800

    Courses are now working.

--- app/models/user_course.rb
+++ app/models/user_course.rb
@@ -1,7 +1,5 @@
 class UserCourse < ActiveRecord::Base
 	enum status: [ :applied, :accepted, :denied ]
-	validates :user_id, uniqueness: true
-	validates :course_id, uniqueness: true
 	belongs_to :user, :foreign_key => "user_id"
 	belongs_to :course, :foreign_key => "course_id"
 end
